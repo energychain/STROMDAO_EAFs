017ddbceb68359bdd50b73aa4c70120eb6656c52
Thorsten Zoerner <me@thorsten-zoerner.com>
2023-12-13 14:56:02 +0100

    v0.2.11 Support for different electricity pricing signal sources

--- framework/public_pwa/assets/js/3party/sw.js
+++ framework/public_pwa/assets/js/3party/sw.js
@@ -2,10 +2,8 @@
 
 const CACHE = "pwabuilder-offline-page";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
