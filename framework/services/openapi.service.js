38f158fb0a17dc82b57cca96e7fe63b80ebd5bed
Thorsten Zoerner <me@thorsten-zoerner.com>
2023-12-31 03:01:24 +0100

    v0.2.31 - Updated Metering API documentation

--- framework/services/openapi.service.js
+++ framework/services/openapi.service.js
@@ -10,7 +10,7 @@ module.exports = {
         // about project
         description: "Energy Applications Framework designed for the German Electricity Market",
         title: "STROMDAO EAF",
-        version: "0.2.30"
+        version: "0.2.31"
       },
       tags: [
         // you tags
