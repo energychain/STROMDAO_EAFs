b8e29e4c3c687cbbe2ace0d22700a5ce068b4078
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-01-02 03:55:43 +0100

    v0.2.33 - Added Invoice Service

--- framework/services/openapi.service.js
+++ framework/services/openapi.service.js
@@ -10,7 +10,7 @@ module.exports = {
         // about project
         description: "Energy Applications Framework designed for the German Electricity Market",
         title: "STROMDAO EAF",
-        version: "0.2.32"
+        version: "0.2.33"
       },
       tags: [
         // you tags
