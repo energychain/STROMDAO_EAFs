48dc97386176e8d55bcf1b17bf9c126c63510aa2
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-01-08 02:06:11 +0100

    v0.2.39 - Allow to run as global script called stromdao-eaf

--- framework/run-eaf.js
+++ framework/run-eaf.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 "use strict";
 
 /**
@@ -6,6 +8,25 @@
  */
 
 const { Runner } = require("moleculer");
+const fs = require("fs");
+console.log(' _____ _______ _____   ____  __  __ _____          ____  ');
+console.log('/ ____|__   __|  __ \ / __ \|  \/  |  __ \   /\   / __ \ ');
+console.log('| (___    | |  | |__) | |  | | \  / | |  | | /  \ | |  | |');
+console.log('\___ \   | |  |  _  /| |  | | |\/| | |  | |/ /\ \| |  | |');
+console.log('____) |  | |  | | \ \| |__| | |  | | |__| / ____ \ |__| |');
+console.log('|_____/   |_|__|_|  \_\\____/|_|  |_|_____/_/    \_\____/ ');
+console.log('           |  ____|   /\   |  ____|                       ');
+console.log('   ______  | |__     /  \  | |__     ______               ');
+console.log('   |______| |  __|   / /\ \ |  __|   |______|             ');
+console.log('           | |____ / ____ \| |                            ');
+console.log('           |______/_/    \_\_|                            ');
+                                                         
+                                                         
+const package_json = JSON.parse(fs.readFileSync("./package.json"));
+console.log("Open-Source Energy Application Framework");
+console.log("License: " + package_json.license);
+console.log("Running: " + package_json.name + " " + package_json.version);
+
 
 const runner = new Runner();
 runner.start(process.argv);
