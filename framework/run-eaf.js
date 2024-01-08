92de377658f9c9e05e5b4c34fa915ab944e206f4
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-01-08 12:09:45 +0100

    v0.2.43 - Answer to life and so on...

--- framework/run-eaf.js
+++ framework/run-eaf.js
@@ -9,6 +9,8 @@
 
 const { Runner } = require("moleculer");
 const fs = require("fs");
+require("dotenv").config();
+
 console.log(' _____ _______ _____   ____  __  __ _____          ____  ');
 console.log('/ ____|__   __|  __ \ / __ \|  \/  |  __ \   /\   / __ \ ');
 console.log('| (___    | |  | |__) | |  | | \  / | |  | | /  \ | |  | |');
@@ -25,33 +27,44 @@ let package_json = {
   license: "Property of STROMDAO GmbH",
   name: "STROMDAO Energy Application Framework (custom build)",
   version: "0.0.1"
-}                
-try {
-  if(fs.existsSync("./package.json")) {
-    package_json = JSON.parse(fs.readFileSync("./package.json"));
-  }
-  if(fs.existsSync("node-modules/stromdao-eaf/package.json")) {
-    package_json = JSON.parse(fs.readFileSync("node-modules/stromdao-eaf/package.json"));
-  }
-} catch(e) {
+}   
+let package_json_file = "package.json";
 
+let workfolder = process.cwd();
+let installfolder = __dirname;
+
+try {
+  if(fs.existsSync(workfolder + "/package.json")) package_json_file = workfolder + "/package.json";
+  if(fs.existsSync(installfolder + "/package.json")) package_json_file = installfolder + "/package.json";
+  package_json = JSON.parse(fs.readFileSync(package_json_file));
+} catch(e) {
+  console.log("Error: " + e);
 }
- 
+
+if((typeof process.env["EAF_WORK"] == 'undefined')||(process.env["EAF_WORK"] == 'null')) process.env["EAF_WORK"] = workfolder;
+if((typeof process.env["EAF_INSTALL"] == 'undefined')||(process.env["EAF_INSTALL"] == 'null')) process.env["EAF_INSTALL"] = installfolder;
+let services = process.env["EAF_INSTALL"]+"/services/**/*.service.js";
+
+if(process.argv[2] == "integration") { 
+  services = process.env["EAF_WORK"]+"/services-integration/**/*.service.js";
+}
+
 console.log("Open-Source Energy Application Framework");
+console.log("--------------------------------------------------");
 console.log("License: " + package_json.license);
 console.log("Running: " + package_json.name + " " + package_json.version);
+console.log("Workfolder: " + process.env["EAF_WORK"]);
+console.log("Installation: "+  process.env["EAF_INSTALL"]);
+console.log("Services: "+  services);
+console.log("Package.json: "+ package_json_file);
+console.log("--------------------------------------------------");
 
+process.chdir(process.env["EAF_INSTALL"]); // Might need to remember original cwd.
 
-process.chdir(__dirname); // Might need to remember original cwd.
-
-/*
-//  Ensure that we find services in the module folder sevices/ and integration services in the cwd.
-for(let i=1;i<process.argv.length;i++) {
-  if(process.argv[i].indexOf("services") == 0) {
-    process.argv[i] = __dirname + "/" + process.argv[i];
-  }
-}
-*/
+let runnerArgs = [];
+runnerArgs.push(process.argv[0]);
+runnerArgs.push(process.argv[1]);
+runnerArgs.push(services);
 
 const runner = new Runner();
-runner.start(process.argv);
+runner.start(runnerArgs);
